9c9cba3b13fa3463c6c4e05255687d2d2c72e725
Eduardo <e.osoriovenegas@gmail.com>
2018-06-06 11:38:40 -0400

    renombrando service.type a service.name

---
+++ db/migrate/20180606153242_cambiarcolumna.rb
@@ -0,0 +1,5 @@
+class Cambiarcolumna < ActiveRecord::Migration[5.2]
+  def change
+  	 rename_column :services, :type, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_18_044404) do
+ActiveRecord::Schema.define(version: 2018_06_06_153242) do
+
+  create_table "offices", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
